daac3c2cbf6cf496b59b7f4fd84c490b6644150c
Roman Katrovsky <katrovsky@gmail.com>
2021-08-15 06:37:53 +0300

    7.1.5 WIP правит отображение дат и маршрута в элементе информации

--- src/utils/route.js
+++ src/utils/route.js
@@ -1,46 +1,5 @@
 import dayjs from 'dayjs';
 
-export const getRoutePrice = (points) => { // вернуть стоимость маршрута
-  let routePrice = 0;
-  points.forEach((point) => {
-    routePrice += point.basePrice; // добавление стоимости поездки
-    point.offers.forEach((offer) => { // добавление стоимости выбранных опций
-      routePrice += offer.price;
-    });
-  });
-  return routePrice;
-};
-
-export const getRouteDates = (point) => { // вернуть время маршрута
-  let routeDates = `${point[0].dateFrom.format('MMM DD')} &nbsp;&mdash;&nbsp ${point[point.length - 1].dateTo.format('MMM DD')}`;
-  if (point[0].dateFrom.format('MMM') === point[point.length - 1].dateTo.format('MMM')) {
-    routeDates = `${point[0].dateFrom.format('MMM DD')} &nbsp;&mdash;&nbsp ${point[point.length - 1].dateTo.format('DD')}`;
-  }
-  return routeDates;
-};
-
-export const getRouteName = (point) => { // вернуть имя маршрута
-  let routeName = `${point[0].destination.name} ... ${point[point.length - 1].destination.name}` ;
-  if (point.length === 3) {
-    routeName = `${point[0].destination.name} &mdash; ${point[1].destination.name}  &mdash; ${point[2].destination.name}`;
-  } else if (point.length === 2) {
-    routeName = `${point[0].destination.name} &mdash; ${point[1].destination.name}`;
-  } else if (point.length === 1) {
-    routeName = `${point[0].destination.name}`;
-  }
-  return routeName;
-};
-
-
-export const getCitiesUniqueNames = (points) => { // выдаёт отсортированный массив уникальных названий городов из массива точек маршрута
-  let citiesNames = new Set();
-  points.forEach((point) => {
-    citiesNames.add(point.destination.name);
-  });
-  citiesNames = Array.from(citiesNames).sort(); // преобразовывает сет в массив, чтобы отсортировать данные по алфавиту
-  return citiesNames;
-};
-
 export const sortByDateFrom = (pointOne, pointTwo) => dayjs(pointOne.dateFrom).diff(dayjs(pointTwo.dateFrom));
 
 export const sortByPrice = (pointOne, pointTwo) => {
@@ -55,3 +14,47 @@ export const sortByDuration = (pointOne, pointTwo) => {
   const secondPointTravelDuration = dayjs(pointTwo.dateTo).diff(dayjs(pointTwo.dateFrom));
   return secondPointTravelDuration - firstPointTravelDuration;
 };
+
+export const getRouteName = (points) => { // вернуть имя маршрута
+  const pointsByDateFrom = points.slice().sort(sortByDateFrom);
+  let routeName = `${pointsByDateFrom[0].destination.name} ... ${pointsByDateFrom[pointsByDateFrom.length - 1].destination.name}` ;
+  if (pointsByDateFrom.length === 3) {
+    routeName = `${pointsByDateFrom[0].destination.name} &mdash; ${pointsByDateFrom[1].destination.name}  &mdash; ${pointsByDateFrom[2].destination.name}`;
+  } else if (pointsByDateFrom.length === 2) {
+    routeName = `${pointsByDateFrom[0].destination.name} &mdash; ${pointsByDateFrom[1].destination.name}`;
+  } else if (pointsByDateFrom.length === 1) {
+    routeName = `${pointsByDateFrom[0].destination.name}`;
+  }
+  return routeName;
+};
+
+export const getRouteDates = (points) => { // вернуть время маршрута
+  const pointsByDateFrom = points.slice().sort(sortByDateFrom);
+  let routeDates = `${pointsByDateFrom[0].dateFrom.format('MMM DD')} &nbsp;&mdash;&nbsp ${pointsByDateFrom[pointsByDateFrom.length - 1].dateTo.format('MMM DD')}`;
+  if (pointsByDateFrom[0].dateFrom.format('MMM') === pointsByDateFrom[pointsByDateFrom.length - 1].dateTo.format('MMM')) {
+    routeDates = `${pointsByDateFrom[0].dateFrom.format('MMM DD')} &nbsp;&mdash;&nbsp ${pointsByDateFrom[pointsByDateFrom.length - 1].dateTo.format('DD')}`;
+  }
+  return routeDates;
+};
+
+export const getRoutePrice = (points) => { // вернуть стоимость маршрута
+  let routePrice = 0;
+  points.forEach((point) => {
+    routePrice += point.basePrice; // добавление стоимости поездки
+    point.offers.forEach((offer) => { // добавление стоимости выбранных опций
+      routePrice += offer.price;
+    });
+  });
+  return routePrice;
+};
+
+export const getCitiesUniqueNames = (points) => { // выдаёт отсортированный массив уникальных названий городов из массива точек маршрута
+  let citiesNames = new Set();
+  points.forEach((point) => {
+    citiesNames.add(point.destination.name);
+  });
+  citiesNames = Array.from(citiesNames).sort(); // преобразовывает сет в массив, чтобы отсортировать данные по алфавиту
+  return citiesNames;
+};
+
+
