5a2b454fb0a4b079e2b413b068aeb9b29ddf0a7f
Roman Katrovsky <katrovsky@gmail.com>
2021-08-21 21:47:59 +0300

    7.1.7 wip начинает делать презентор фильтров

---
+++ src/presenter/filter.js
@@ -0,0 +1,5 @@
+import FilterView from '../view/site-menu.js';
+import { render, RenderPosition, replace, remove } from '../utils/render.js';
+import { filter } from '../utils/filter.js';
+import { FilterType, UpdateType } from '../utils/const.js';
+
